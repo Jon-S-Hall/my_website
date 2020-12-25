2f0432a795ab085154c08002fcf7327373cd5b84
jonat <jshall@bu.edu>
2020-12-24 21:47:04 -0500

    initial font changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-
+//this function basically is present in all pages.This can be used to keep state across pages.
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
