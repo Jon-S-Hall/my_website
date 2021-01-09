d599677cf2ec9a66d5f554e575398a9b90693670
Jonathan Hall <jonathanhall456@gmail.com>
2021-01-09 00:47:12 -0500

    made changes to V4 pages

--- pages/projects/VR_4D.js
+++ pages/projects/VR_4D.js
@@ -1,15 +1,29 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../../components/Layout'
-import styles from '../../styles/Projects.module.css'
+import styles from '../../styles/Project.module.css'
 import utilStyles from '../../styles/utils.module.css'
 
 export default function VR_4D() {
     return (
         <Layout>
-            <main className={styles.proj_main}>
-                <h1 className={utilStyles.headingLg}>
-                Sorry! Page Under Construction.
-                </h1>
+            <main className={styles.body}>
+                <div className={styles.banner}>
+                    <img className={styles.logo} src={"/LORELAD.png"}/>
+                </div>
+                <body className={styles.body}>
+                <section className={styles.top_section}>
+                    <div className={styles.text_div}>
+                        <p className={utilStyles.headingXl}>
+                            Abstract
+                        </p>
+                        <p className={utilStyles.bodyMd}>
+                            The Low Resource Language Database (LORELAD) is a project that I designed to aid language preservation efforts. The project was selected to be part of the
+                            BU Spark! Innovation Fellowship for the Fall 2020 semester. Along with guidance and funding, I got to interview and select 4 teammates to lead in
+                            developing Lorelad.
+                        </p>
+                    </div>
+                </section>
+                </body>
             </main>
         </Layout>
     )
