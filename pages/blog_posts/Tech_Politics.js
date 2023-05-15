ffae09193c27d2278153387a0e08cd61670d4610
Jonathan Hall <jshall@bu.edu>
2023-05-14 23:33:02 -0700

    bugfix for build

--- pages/blog_posts/Tech_Politics.js
+++ pages/blog_posts/Tech_Politics.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/Layout'
-import styles from '../styles/Projects.module.css'
-import utilStyles from '../styles/utils.module.css'
+import Layout, { siteTitle } from '../../components/Layout'
+import styles from '../../styles/Projects.module.css'
+import utilStyles from '../../styles/utils.module.css'
 import dynamic from "next/dynamic";
 
 export default function Tech_Politics() {
